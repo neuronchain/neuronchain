5ab64e81490293777ce231cd91de5268ba26d99d
Oleg <mrB4el@hotmail.com>
2018-01-22 16:53:08 +0700

    global_properties

--- libraries/chain/include/graphene/chain/config.hpp
+++ libraries/chain/include/graphene/chain/config.hpp
@@ -92,6 +92,7 @@
 #define GRAPHENE_DEFAULT_MARGIN_PERIOD_SEC              (30*60*60*24)
 
 #define GRAPHENE_DEFAULT_MIN_WITNESS_COUNT                    (11)
+#define GRAPHENE_DEFAULT_RANDOM_WITNESS_COUNT                 (2)
 #define GRAPHENE_DEFAULT_MIN_COMMITTEE_MEMBER_COUNT           (11)
 #define GRAPHENE_DEFAULT_MAX_WITNESSES                        (1001) // SHOULD BE ODD
 #define GRAPHENE_DEFAULT_MAX_COMMITTEE                        (1001) // SHOULD BE ODD
